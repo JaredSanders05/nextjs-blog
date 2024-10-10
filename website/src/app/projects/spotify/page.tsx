2ab05ce4887889a46b94598dfba428a4f74efa83
Skullblitz <Jaredsanders1127@gmail.com>
2024-10-09 21:45:53 -0500

    Update page.tsx

--- website/src/app/projects/spotify/page.tsx
+++ website/src/app/projects/spotify/page.tsx
@@ -1,2 +1,14 @@
 // MAKE A PAGE THAT SHOWS WHAT I AM LISTENING TO ON SPOTIFY 
 // Have it show the lyrics and a card of the song and have it beable to flip over to show the artist
+
+
+const App: React.FC = () => {
+    return (
+      <div>
+        <p>Test</p>
+      </div>
+    );
+  };
+  
+  export default App;
+  

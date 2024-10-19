154a3cf9747348cbbf382c4bcb280e3ef21d8d21
Skullblitz <Jaredsanders1127@gmail.com>
2024-10-19 13:27:47 -0500

    Update Spotify.js

--- pages/api/Spotify.js
+++ pages/api/Spotify.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const CLIENT_ID = "7d795eebfbe44d3db59d5691006cd34d";
 const YOUR_CLIENT_SECRET = "c1811e7d4cee4efbbe598a9d1140d0d2";
-const REDIRECT_URI = "http://localhost:3000";
+const REDIRECT_URI = "jaredsanders.vercel.app";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "code";
 const SCOPE = 'user-read-currently-playing user-top-read';
